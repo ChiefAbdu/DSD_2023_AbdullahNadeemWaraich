c3792333840172f65a4e72b6cfdba6fe0599535c
ChiefAbdu <abdullahwaraich2004@gmail.com>
2025-02-16 23:43:59 +0500

    Add files via upload

---
+++ Lab_no4/readme.md
@@ -0,0 +1,9 @@
+# Lab Assigment 4 
+## Task 1
+1. Using 2 Different 2 bits input (a,b) to power an RGB light to create Purple,Yellow and Cyan.
+2. Creating K-Maps of three outputs (red,green,blue) and creating an equation using the prime implicants.
+3. Creating Circuit Diagrams Using the found equations.
+4. Running a simulation and comparing the diagrams.
+5. Finding LUTs Used and IOs Used.
+## Task 2
+System Verilog Code of the Circuit
---
+++ Lab_no4/rtl/tricolor.sv
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module tricolor(
+    input logic [1:0] a,
+    input logic [1:0] b,
+    output logic red,green,blue 
+);
+
+    assign red = (a[1] & ~b[0]) | (a[1] & ~b[1]) | (a[0] & a[1]) | (~b[0] & ~b[1]) | (a[0] & ~b[1]);
+    
+    assign blue = (~a[0] & b[0]) | (a[1] & ~b[1]) | (a[0] & ~b[0]) | (~a[1] & b[1]);
+
+    assign green = (~a[0] & ~a[1]) | (~a[0] & b[1]) | (b[0] & b[1]) | (~a[1] & b[1]) | (~a[1] & b[0]);
+
+endmodule
+
